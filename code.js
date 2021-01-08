33e5f43435748cbe978c59a3a8c90a59d2886ebc
Pradeep Kumar Sahani <mark42virus>
2021-01-08 14:47:30 +0530

    updation

---
+++ code.js
@@ -0,0 +1,9 @@
+
+checkAge = () =>{
+    var ageYear = document.getElementById('num1');
+var currentYear = document.getElementById('num2');
+
+var data  = currentYear-ageYear;
+console.log(data);
+
+}
